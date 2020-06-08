5b464079c2a1c9227b097935881e553bf5da8b8f
David Avenante <altus34@gmail.com>
2020-06-07 21:36:27 -0400

    Add code climate (#3)
    Co-authored-by: David <altus@gmail.com>

---
+++ .github/actions/code-coverage.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter \
+
+cp ./core/target/site/jacoco/jacoco.xml ./core/src/main/groovy/jacoco.xml
+cp ./web/target/site/jacoco/jacoco.xml ./web/src/main/groovy/jacoco.xml
+
+CURRENT_DIR=$PWD
+
+cd ./core/src/main/groovy/
+../../../../cc-test-reporter format-coverage -t jacoco -o codeclimate.core.json jacoco.xml
+mv codeclimate.core.json ${CURRENT_DIR}
+rm jacoco.xml
+
+cd ${CURRENT_DIR}
+cd ./web/src/main/groovy/
+../../../../cc-test-reporter format-coverage -t jacoco -o codeclimate.web.json jacoco.xml
+mv codeclimate.web.json ${CURRENT_DIR}
+rm jacoco.xml
+
+cd ${CURRENT_DIR}
+./cc-test-reporter sum-coverage codeclimate.*.json -p 2
+./cc-test-reporter upload-coverage
