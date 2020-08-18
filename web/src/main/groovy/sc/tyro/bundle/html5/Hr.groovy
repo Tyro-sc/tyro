ced01f4b51e562235a799cf5a2ac5e7a2eac600b
altus34 <altus34@gmail.com>
2020-08-18 14:48:42 -0400

    Work on HTML components

---
+++ web/src/main/groovy/sc/tyro/bundle/html5/Hr.groovy
@@ -0,0 +1,12 @@
+package sc.tyro.bundle.html5
+
+import sc.tyro.core.component.Component
+import sc.tyro.web.CssIdentifier
+
+/**
+ * @author David Avenante
+ * @since 1.0.0
+ */
+@CssIdentifier('hr')
+class Hr extends Component {
+}
---
+++ web/src/main/groovy/sc/tyro/bundle/html5/Pre.groovy
@@ -0,0 +1,17 @@
+package sc.tyro.bundle.html5
+
+import sc.tyro.core.component.Component
+import sc.tyro.core.support.property.TextSupport
+import sc.tyro.web.CssIdentifier
+
+/**
+ * @author David Avenante
+ * @since 1.0.0
+ */
+@CssIdentifier('pre')
+class Pre extends Component implements TextSupport {
+    @Override
+    String text() {
+        provider.eval(id(), "it.text()")
+    }
+}
