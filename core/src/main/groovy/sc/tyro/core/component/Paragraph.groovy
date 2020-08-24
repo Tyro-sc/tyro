2eba65c89dcbbdcbfa933ae7dcbc3433844fde56
David Avenante <altus34@gmail.com>
2020-08-24 18:09:04 -0400

    Add paragraph component (#13)
    * Add Paragraph component

---
+++ core/src/main/groovy/sc/tyro/core/component/Paragraph.groovy
@@ -0,0 +1,9 @@
+package sc.tyro.core.component
+
+import sc.tyro.core.support.property.TextSupport
+
+/**
+ * @author David Avenante
+ * @since 1.0.0
+ */
+abstract class Paragraph extends Component implements TextSupport {}
