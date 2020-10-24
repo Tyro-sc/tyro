c0056a9dac9c91231914b26b7d19925819c70b0e
David Avenante <altus34@gmail.com>
2020-10-24 19:24:59 -0400

    Clean CI (#17)

--- .github/actions/code-coverage.sh
+++ .github/actions/code-coverage.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+. ${PWD}/.github/actions/logger.sh
+
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter \
 
