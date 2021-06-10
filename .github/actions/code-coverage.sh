3ea051bfd9547dbe0c7fe6981fdff1d4733c0088
altus34 <d.avenante@gmail.com>
2021-06-10 19:33:04 -0400

    Improve Code coverage config

--- .github/actions/code-coverage.sh
+++ .github/actions/code-coverage.sh
@@ -8,6 +8,8 @@ chmod +x ./cc-test-reporter \
 MODULES=(core web)
 CURRENT_DIR=$PWD
 
+./cc-test-reporter before-build
+
 for module in "${MODULES[@]}"; do
   cp "./${module}/target/site/jacoco/jacoco.xml" "./${module}/src/main/groovy/jacoco.xml"
   cd "./${module}/src/main/groovy/" || exit
@@ -21,3 +23,4 @@ done
 cd "${CURRENT_DIR}" || exit
 ./cc-test-reporter sum-coverage codeclimate.*.json -p 2
 ./cc-test-reporter upload-coverage
+./cc-test-reporter after-build
