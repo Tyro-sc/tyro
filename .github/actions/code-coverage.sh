ae2fcbbb5809967a9542457819ee2941fbb7d867
altus34 <d.avenante@gmail.com>
2021-06-10 22:52:42 -0400

    Improve Code coverage config

--- .github/actions/code-coverage.sh
+++ .github/actions/code-coverage.sh
@@ -2,32 +2,24 @@
 
 . ${PWD}/.github/actions/logger.sh
 
+#GITHUB_WORKSPACE=~/Projects/Tyro-sc/tyro
+
+cd "${GITHUB_WORKSPACE}" || exit
+
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter \
 
 ./cc-test-reporter before-build
 
-COVERAGE_FILE="./core/target/site/jacoco/jacoco.xml"
-./cc-test-reporter format-coverage --input-type jacoco --output "codeclimate.core.json"
+cp "${GITHUB_WORKSPACE}/core/target/site/jacoco/jacoco.xml" "${GITHUB_WORKSPACE}/core/src/main/groovy/jacoco.xml"
+cd "${GITHUB_WORKSPACE}/core/src/main/groovy" || exit
+../../../../cc-test-reporter format-coverage -d --input-type jacoco --output "../../../../codeclimate.core.json"
 
-COVERAGE_FILE="./web/target/site/jacoco/jacoco.xml"
-./cc-test-reporter format-coverage --input-type jacoco --output "codeclimate.web.json"
+cp "${GITHUB_WORKSPACE}/web/target/site/jacoco/jacoco.xml" "${GITHUB_WORKSPACE}/web/src/main/groovy/jacoco.xml"
+cd "${GITHUB_WORKSPACE}/web/src/main/groovy" || exit
+../../../../cc-test-reporter format-coverage -d --input-type jacoco --output "../../../../codeclimate.web.json"
 
-./cc-test-reporter sum-coverage codeclimate.*.json -p 2 --output codeclimate.json
+cd "${GITHUB_WORKSPACE}" || exit
+./cc-test-reporter sum-coverage codeclimate.*.json --parts 2 --output codeclimate.json
 ./cc-test-reporter upload-coverage --input codeclimate.json
 ./cc-test-reporter after-build --coverage-input-type jacoco --exit-code $?
-
-#
-#for module in "${MODULES[@]}"; do
-#  cp "./${module}/target/site/jacoco/jacoco.xml" "./${module}/src/main/groovy/jacoco.xml"
-#  cd "./${module}/src/main/groovy/" || exit
-#  ../../../../cc-test-reporter format-coverage -t jacoco -o "codeclimate.${module}.json" jacoco.xml
-#  mv "codeclimate.${module}.json" "${CURRENT_DIR}"
-#  rm jacoco.xml
-#
-#  cd "${CURRENT_DIR}" || exit
-#done
-#
-#cd "${CURRENT_DIR}" || exit
-#./cc-test-reporter sum-coverage codeclimate.*.json -p 2
-#./cc-test-reporter upload-coverage
