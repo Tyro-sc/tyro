8025b7bdb0499d5d8b8e9bfe0bc3e9fb70a18980
altus34 <d.avenante@gmail.com>
2021-06-10 20:56:51 -0400

    Improve Code coverage config

--- .github/actions/code-coverage.sh
+++ .github/actions/code-coverage.sh
@@ -11,16 +11,25 @@ CURRENT_DIR=$PWD
 ./cc-test-reporter before-build
 
 for module in "${MODULES[@]}"; do
-  cp "./${module}/target/site/jacoco/jacoco.xml" "./${module}/src/main/groovy/jacoco.xml"
-  cd "./${module}/src/main/groovy/" || exit
-  ../../../../cc-test-reporter format-coverage -t jacoco -o "codeclimate.${module}.json" jacoco.xml
-  mv "codeclimate.${module}.json" "${CURRENT_DIR}"
-  rm jacoco.xml
-
-  cd "${CURRENT_DIR}" || exit
+  COVERAGE_FILE="./${module}/target/site/jacoco/jacoco.xml"
+  ./cc-test-reporter format-coverage --input-type jacoco --output "codeclimate.${module}.json"
 done
 
-cd "${CURRENT_DIR}" || exit
-./cc-test-reporter sum-coverage codeclimate.*.json -p 2
-./cc-test-reporter upload-coverage
+./cc-test-reporter sum-coverage--output codeclimate.json codeclimate.*.json
+./cc-test-reporter upload-coverage --input codeclimate.json
 ./cc-test-reporter after-build --coverage-input-type jacoco --exit-code $?
+
+#
+#for module in "${MODULES[@]}"; do
+#  cp "./${module}/target/site/jacoco/jacoco.xml" "./${module}/src/main/groovy/jacoco.xml"
+#  cd "./${module}/src/main/groovy/" || exit
+#  ../../../../cc-test-reporter format-coverage -t jacoco -o "codeclimate.${module}.json" jacoco.xml
+#  mv "codeclimate.${module}.json" "${CURRENT_DIR}"
+#  rm jacoco.xml
+#
+#  cd "${CURRENT_DIR}" || exit
+#done
+#
+#cd "${CURRENT_DIR}" || exit
+#./cc-test-reporter sum-coverage codeclimate.*.json -p 2
+#./cc-test-reporter upload-coverage
